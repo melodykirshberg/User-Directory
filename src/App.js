57dd19491dc65d89612099eb42dbb34f6b1e5799
verlitas <55808608+verlitas@users.noreply.github.com>
2019-12-22 19:15:55 -0700

    got table working

--- src/App.js
+++ src/App.js
@@ -12,27 +12,43 @@ const App = () => {
   }, []);
 
   const sortAscending = () => {
-    const sortedUsers = [...users].sort((a,b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);
+    const sortedUsers = [...users].sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);
     setUsers(sortedUsers)
   };
   const sortDescending = () => {
-    const sortedUsers = [...users].sort((a,b) => a.name.last.toLowerCase() < b.name.last.toLowerCase() ? 1 : -1);
+    const sortedUsers = [...users].sort((a, b) => a.name.last.toLowerCase() < b.name.last.toLowerCase() ? 1 : -1);
     setUsers(sortedUsers);
   };
   return (
     <>
-    <input type="text" value={inputValue} onChange={event => setInputValue(event.target.value)}/>
-    <button onClick={sortAscending}>SORT A-Z BY LAST NAME</button>
-    <button onClick={sortDescending}>SORT Z-A BY LAST NAME</button>
-    {users
-    .filter(user => 
-      `${user.name.first} ${user.name.last}`
-      .toLowerCase()
-      .includes(inputValue.toLowerCase()))
-      .map(user => (
-        <h1 key={user.id.value}>
-          {user.name.first} {user.name.last}, {user.login.username}, {" "} {user.email}
-        </h1>
+      <input type="text" value={inputValue} onChange={event => setInputValue(event.target.value)} />
+      <button onClick={sortAscending}>SORT A-Z BY LAST NAME</button>
+      <button onClick={sortDescending}>SORT Z-A BY LAST NAME</button>
+      {users
+        .filter(user =>
+          `${user.name.first} ${user.name.last}`
+            .toLowerCase()
+            .includes(inputValue.toLowerCase()))
+        .map(user => (
+          <div key={user.id.value}>
+
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{user.name.first + (' ') + user.name.last}</td>
+                <td>{user.login.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
       ))}
     </>
   )
